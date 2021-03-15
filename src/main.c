30657049e8125c71bd485eeaef534ab29e763f92
danielmurphy <dm4g16@soton.ac.uk>
2021-03-15 13:04:02 +0000

    Added a hal folder and changed main loop
    Added a loop for the msp to run in in the main.c file, which will exit
    according to interrupts
    
    Added a hal folder with empty source and header files up to date with
    the sw architechture

--- src/main.c
+++ src/main.c
@@ -6,7 +6,33 @@
  */
 int main(void)
 {
+<<<<<<< HEAD
+	WDTCTL = WDTPW | WDTHOLD;	/* stop watchdog timer */
+
+	/*initialise hardware/functions */
+
+	/* need to contain main loop which will run the msp430 in sleep mode and
+	 * then exit in case of any of the interrupts and carry out the required process*/
+	while(true){
+	    switch(SLEEP MODE){
+	    case UART Comms:
+	        TOBC Comms Process;
+	        break;
+	    case Timer:
+	        Timer:
+	        Timer Interrupt Process;
+	        break;
+	    case OCP Event:
+	        OCP Notification Process;
+	        break;
+	    case TOBC Interrupt:
+	        TOBC Interrupt Process;
+	        break;
+	    }
+	}
+=======
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+>>>>>>> refs/remotes/origin/GDP202021
 	
 	return 0;
 }
