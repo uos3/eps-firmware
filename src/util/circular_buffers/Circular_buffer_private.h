fc9a58955f5b7ea01d4a46a5efab588be08f7795
danielmurphy <dm4g16@soton.ac.uk>
2021-03-21 13:35:13 +0000

    UART driver mainly finished, just need to add in a few global defines

---
+++ src/util/circular_buffers/Circular_buffer_private.h
@@ -0,0 +1,16 @@
+/*
+ * Circular_buffer_private.h
+ *
+ *  Created on: 19 Mar 2021
+ *      Author: danielmurphy
+ */
+
+#ifndef UTIL_CIRCULAR_BUFFERS_CIRCULAR_BUFFER_PRIVATE_H_
+#define UTIL_CIRCULAR_BUFFERS_CIRCULAR_BUFFER_PRIVATE_H_
+
+#include <stdint.h>
+
+uint8_t Circular_buffer_length(circ *p_c);
+
+
+#endif /* UTIL_CIRCULAR_BUFFERS_CIRCULAR_BUFFER_PRIVATE_H_ */
