7746c87ef46835e1b57eb1cf537e3ad02a3c86e6
Roberto <rh3u17@soton.ac.uk>
2021-03-29 17:47:48 +0100

    Created Event Handler files.

---
+++ src/event_handlers/ocp/Ocp_public.c
@@ -0,0 +1,12 @@
+/*
+ * ocp_public.c
+ *
+ *  Created on: 29 Mar 2021
+ *      Author: Roberto
+ */
+
+#include "ocp_public.h"
+
+int Ocp_event(){
+    return 1;
+}
---
+++ src/event_handlers/tobc_pin/TobcPin_public.c
@@ -0,0 +1,14 @@
+/*
+ * Tobc_public.c
+ *
+ *  Created on: 29 Mar 2021
+ *      Author: Roberto
+ */
+
+#include "TobcPin_public.h"
+
+int TobcPin_event(){
+    return 0;
+}
+
+
