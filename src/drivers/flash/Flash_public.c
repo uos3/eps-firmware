f8bd169dd2d748ba703d21e63fd5da6c28067b7f
danielmurphy <dm4g16@soton.ac.uk>
2021-04-10 22:37:35 +0100

    Added Flash driver

--- src/drivers/flash/Flash_public.c
+++ src/drivers/flash/Flash_public.c
@@ -1,10 +1,65 @@
 /*
- * Flash_public.c
+ * @ingroup flash
  *
- *  Created on: 15 Mar 2021
- *      Author: Roberto
+ * @file Flash_public.c
+ * @author Daniel Murphy (dm4g16@soton.ac.uk)
+ * @brief Public Flash driver source file
+ *
+ * Implements Flash driver for the MSP4302132
+ *
+ * @version 0.1
+ * @date 2021-04-10
+ *
+ * @copyright UOS3
  */
 
+/* -------------------------------------------------------------------------
+ * INCLUDES
+ * ------------------------------------------------------------------------- */
 
+/* Standard library includes */
+#include <stdio.h>
+#include <stdint.h>
+
+
+/* Internal includes */
+#include <msp430.h>
+
+/* -------------------------------------------------------------------------
+ * FUNCTIONS
+ * ------------------------------------------------------------------------- */
+uint8_t Flash_init(void) {
+    /*@brief initialises flash, sets timing generator to 400kHz (8MHz / 20)*/
+    FCTL2 |= FSSEL_2 + FN4 + FN1 + FN0;
+    return 0;
+}
+
+uint8_t Flash_read(uint8_t address, uint8_t bytecount_in, uint8_t *p_data_out) {
+    /*@brief sets the address and reads the data from the flash*/
+    int i;
+    uint8_t *p_Flash (uint8_t *) address;
+    for(i = 0; i < bytecount_in; i ++) {
+        *p_data_out = *p_Flash;
+        p_Flash++;
+        p_data_out++;
+    }
+    return 0;
+}
+
+uint8_t Flash_write(uint8_t address, uint8_t bytecount_in, uint8_t *p_data_in) {
+    /*@brief writes to the flash by first erasing, and then writing the data from p_data_in*/
+    int i;
+    uint8_t *p_Flash (uint8_t *) address;
+    FCTL3 = FWKEY;
+    FCTL1 = FWKEY + ERASE;
+    *p_Flash = 0;
+    FCTL1 = FWKEY + WRT;
+    for(i = 0; i <bytecount_in; i ++) {
+        *p_Flash = *p_data_in;
+        p_Flash++;
+        p_data_in++;
+    }
+    return 0;
+}
 
 
--- src/drivers/flash/Flash_public.h
+++ src/drivers/flash/Flash_public.h
@@ -1,14 +1,39 @@
 /*
- * Flash_public.h
+ * @ingroup flash
  *
- *  Created on: 15 Mar 2021
- *      Author: Roberto
+ * @file Flash_public.c
+ * @author Daniel Murphy (dm4g16@soton.ac.uk)
+ * @brief Public Flash driver source file
+ *
+ * Implements Flash driver for the MSP4302132
+ *
+ * @version 0.1
+ * @date 2021-04-10
+ *
+ * @copyright UOS3
  */
 
 #ifndef DRIVERS_FLASH_FLASH_PUBLIC_H_
 #define DRIVERS_FLASH_FLASH_PUBLIC_H_
 
+/* -------------------------------------------------------------------------
+ * INCLUDES
+ * ------------------------------------------------------------------------- */
 
+/* Standard library includes */
+#include <stdio.h>
+#include <stdint.h>
+
+
+/* Internal includes */
+#include <msp430.h>
+
+/* -------------------------------------------------------------------------
+ * FUNCTIONS
+ * ------------------------------------------------------------------------- */
+uint8_t Flash_init(void);
+uint8_t Flash_read(uint8_t address, uint8_t bytecount_in, uint8_t *p_data_out);
+uint8_t Flash_write(uint8_t address, uint8_t bytecount_in, uint8_t *p_data_in);
 
 
 
