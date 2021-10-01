9eff786aad2ca0f103bdfc5847f6ff6f62130470
[Oliver Perez] <[opf1u20@soton.ac.uk]>
2021-10-01 14:59:01 +0100

    [Git setup demo Oliver]

--- src/applications/battery_comms/BatteryComms_public.c
+++ src/applications/battery_comms/BatteryComms_public.c
@@ -16,6 +16,7 @@
 uint16_t BatteryComms_TX_RX(uint8_t command_in, uint16_t data_in) {
     Battery_TX(command_in, data_in);
 
+//hello my name is Oli and this a git test
     /* Only expect a response if a manual reset was not ordered */
     if (command_in != 0x80) {
         return Battery_RX();
